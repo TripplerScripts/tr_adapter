4d7270f5dc2921c536fbee2eaf8149a8944c8fe9
Lenix <lenixdev@users.noreply.github.com>
2025-09-30 23:30:49 +0100

    improve: add wrappers

--- mapper/server.lua
+++ mapper/server.lua
@@ -1,21 +1,21 @@
 function MapArguments(inputArgs, fromArgsConfig, toArgsConfig)
   local mappedArgs = {}
-  
+
   local fromLookup = {}
   for i, argConfig in ipairs(fromArgsConfig) do
       fromLookup[argConfig.name] = i
   end
-  
+
   for i, argConfig in ipairs(toArgsConfig) do
       local argName = argConfig.name
       local defaultValue = argConfig.defaultValue
-      
+
       if fromLookup[argName] and inputArgs[fromLookup[argName]] ~= nil then
           mappedArgs[i] = inputArgs[fromLookup[argName]]
       elseif defaultValue ~= nil then
           mappedArgs[i] = defaultValue
       end
   end
-  
+
   return mappedArgs
 end
