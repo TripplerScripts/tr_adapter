72180fbe71fa6c8d8ce698d695836f3c9932bb16
Lenix <lenixdev@users.noreply.github.com>
2025-09-30 17:22:46 +0100

    update: removed ps-inventory since its not different and unuseful for dev

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -16,13 +16,12 @@ server_scripts {
     'selector/server.lua',
     'detecter/extractor/server.lua',
     'detecter/resources/server.lua',
-    'mapper/server.lua'
+    'mapper/server.lua',
+    'converter/server.lua',
 }
 files {
     'compatibilities/inventory/_init/server.lua',
     'compatibilities/inventory/ox_inventory/server.lua',
     'compatibilities/inventory/qb-inventory/server.lua',
-    'compatibilities/inventory/ps-inventory/server.lua',
 }
 provide 'ox_inventory'
-provide 'ps-inventory'
